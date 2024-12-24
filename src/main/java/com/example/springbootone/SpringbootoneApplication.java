9b747822d650239c6d24db7d561a099c8c4661c1
Koushik5452 <annemkoushikreddy5452@gmail.com>
2024-12-24 23:32:41 +0530

    task1

--- src/main/java/com/example/springbootone/SpringbootoneApplication.java
+++ src/main/java/com/example/springbootone/SpringbootoneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootoneApplication {
 
 	public static void main(String[] args) {
+		//hii koushik
 		SpringApplication.run(SpringbootoneApplication.class, args);
 	}
 
